a494129ff40a873d8675c046bd7a0f84f98871e8
dn-37 <dn375@mail.ru>
2024-07-27 11:27:44 +0400

    made component filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { Title } from "./title";
 import { FilterCheckbox } from "./filter-checkbox";
 import { Input } from "../ui";
 import { RangeSlider } from "./range-slider";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
 
 interface Props {
   className?: string;
@@ -19,17 +20,105 @@ export const Filters: React.FC<Props> = ({ className }) => {
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3">Цена от и до:</p>
         <div className="flex gap-3 mb-5">
-          <Input type="number" placeholder="0" min={0} max={1000} value={0} />
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
           <Input
             type="number"
             min={100}
             max={1000}
             placeholder="1000"
-            value={1000}
+            defaultValue={1000}
           />
         </div>
+        <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
       </div>
-      <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        name="ingredients"
+        className="mt-5"
+        limit={6}
+        items={[
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моцарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томат",
+            value: "6",
+          },
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моцарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томат",
+            value: "6",
+          },
+        ]}
+        defaultItems={[
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моцарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томат",
+            value: "6",
+          },
+        ]}
+      />
     </div>
   );
 };
