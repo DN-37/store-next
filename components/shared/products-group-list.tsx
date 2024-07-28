de6763b490a252e66144c118c2c06e7e2a9d0d2f
dn-37 <dn375@mail.ru>
2024-07-28 12:31:14 +0400

    added zustand and intersection observer

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -6,6 +6,7 @@ import { useIntersection } from "react-use";
 import { Title } from "./title";
 import { cn } from "@/lib/utils";
 import { ProductCard } from "./product-card";
+import { useCategoryStore } from "@/store/category";
 
 interface Props {
   title: string;
@@ -22,8 +23,19 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryId,
   className,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.3,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
   return (
-    <div className={className} id={title}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
