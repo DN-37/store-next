d953cb28294537f41d97ecd9ad0a4218c602d0f5
dn-37 <dn375@mail.ru>
2024-07-30 18:42:07 +0400

    created seed

---
+++ prisma/seed.ts
@@ -0,0 +1,53 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "./prisma-client";
+import { products, categories } from "./constants";
+import { hashSync } from "bcrypt";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User Test",
+        email: "user@test.ru",
+        password: hashSync("111111", 10),
+      },
+      {
+        fullName: "Admin Admin",
+        email: "admin@test.ru",
+        password: hashSync("111111", 10),
+      },
+    ],
+  });
+
+  await prisma.category.createMany({
+    data: categories,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
