3e1dace383ad5bc0776e855d7dfebe426b3ef772
dn-37 <dn375@mail.ru>
2024-08-08 18:19:46 +0400

    remade structure project

---
+++ shared/components/shared/index.ts
@@ -0,0 +1,16 @@
+export { Container } from "./container";
+export { Header } from "./header";
+export { Title } from "./title";
+export { Categories } from "./categories";
+export { SortPopup } from "./sort-popup";
+export { TopBar } from "./top-bar";
+export { Filters } from "./filters";
+export { FilterCheckbox } from "./filter-checkbox";
+export { RangeSlider } from "./range-slider";
+export { CheckboxFiltersGroup } from "./checkbox-filters-group";
+export { PizzaImage } from "./pizza-image";
+export { GroupVariants } from "./group-variants";
+export { ChooseProductForm } from "./choose-product-form";
+export { IngredientItem } from "./ingredient-item";
+export { ProductForm } from "./product-form";
+export * from "./modals";
