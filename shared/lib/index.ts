2c9f3a9cbb8c7171b88a58f9e2e366521499b5ed
dn-37 <dn375@mail.ru>
2024-08-14 18:58:26 +0400

    made api for cart

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -4,3 +4,5 @@ export { getPizzaDetails } from "./get-pizza-details";
 export { getCartDetails } from "./get-cart-details";
 export { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
 export { getCartItemDetails } from "./get-cart-item-details";
+export { updateCartTotalAmount } from "./update-cart-total-amount";
+export { findOrCreateCart } from "./find-or-create-cart";
